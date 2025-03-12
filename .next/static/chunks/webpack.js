01d073a21f2188ca07d3605468418dd775672022
DevGilvanJunior <gsjunior.14@gmail.com>
2025-03-12 04:31:22 -0300

    atualizando arquivo index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "839d7243f24aace2"; }
+/******/ 		__webpack_require__.h = function() { return "1cecfb8cf354b1b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>teste 2</h1>
+    return <h1>Meu primeiro projeto Next JS, consegue visualizar?</h1>
 }
 
 export default Home;
