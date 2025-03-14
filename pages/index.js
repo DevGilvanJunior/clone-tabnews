cee70c41ac624ed64f70df8db4d73504cad6a966
DevGilvanJunior <gsjunior.14@gmail.com>
2025-03-14 05:11:11 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Meu primeiro projeto Next JS, consegue visualizar?</h1>
+    return <h1>Meu primeiro projeto Next JS, consegue visualizar? Será que deu certo?</h1>
 }
 
 export default Home;
